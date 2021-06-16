77652e706c2585d5dca315f70335f69ef5ab5532
Jadfii <Jadfii@users.noreply.github.com>
2021-06-17 00:05:59 +0100

    Barrel length now affects camera transition time

--- Scripts/4_World/entities/manbase/dayzplayer/dayzplayercameras.c
+++ Scripts/4_World/entities/manbase/dayzplayer/dayzplayercameras.c
@@ -3,8 +3,8 @@ modded class DayZPlayerCameras
 	static const float TIME_CAMERACHANGE_IRONSIGHTS = 0.25;
 	static const float TIME_CAMERACHANGE_OPTICS = 0.3;
 
-	static const float TIME_CAMERACHANGE_MIN = 0.225;
-	static const float TIME_CAMERACHANGE_MAX = 0.35;
+	static const float TIME_CAMERACHANGE_MIN = 0.2;
+	static const float TIME_CAMERACHANGE_MAX = 0.4;
 
 	/* It's unfortunate that static methods have to be overriden, not modded with super :( */
 
@@ -39,11 +39,15 @@ modded class DayZPlayerCameras
 		Weapon_Base weapon;
 		if (!Class.CastTo(weapon, player.GetItemInHands())) return transTime;
 
-		float weapon_weight = (weapon.GetWeight() / 1000) * 0.3;
+		float weapon_weight = (weapon.GetWeight() / 1000) * 0.5;
 		transTime *= weapon_weight;
+
+		float barrel_length = weapon.GetWeaponLength();
+		transTime *= barrel_length;
 		
 		transTime = Math.Clamp(transTime, TIME_CAMERACHANGE_MIN, TIME_CAMERACHANGE_MAX);
 
+		DbgPrintPlayerCameras("barrel_length: "+barrel_length);
 		DbgPrintPlayerCameras("weapon_weight: "+weapon_weight);
 		DbgPrintPlayerCameras("transTime: "+transTime);
 
