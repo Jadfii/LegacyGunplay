9bfca9445918cd68dfa187d22e3de93ad1778a12
Jadfii <Jadfii@users.noreply.github.com>
2021-06-16 23:31:59 +0100

    Show body clothing item when scope FOV is high to prevent invisible arms

--- Scripts/4_World/entities/manbase/playerbase.c
+++ Scripts/4_World/entities/manbase/playerbase.c
@@ -47,8 +47,18 @@ modded class PlayerBase extends ManBase
 		}
 	}
 
-	/*override void HideClothing(ItemOptics optic, bool state)
+	override void HideClothing(ItemOptics optic, bool state)
 	{
-		return;
-	}*/
+		super.HideClothing(optic, state);
+		float default_fov = 30 * Math.DEG2RAD;
+		array<int> clothingArray = new array<int>;
+
+		// I don't want to NOT call super, so we're just going to re-enable
+		// some clothing pieces if they shouldn't be invisible
+		if (state && DayZPlayerCameraOptics.Cast(m_CurrentCamera) && GetInstanceType() == DayZPlayerInstanceType.INSTANCETYPE_CLIENT && optic.GetCurrentStepFOV() > default_fov)
+		{
+			clothingArray.Insert(InventorySlots.BODY);
+			SetInvisibleRecursive(false, this, clothingArray);
+		}
+	}
 }
