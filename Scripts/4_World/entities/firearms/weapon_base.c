95b8076dbfa963e763e035c5e87b6848a5c9490a
Jadfii <Jadfii@users.noreply.github.com>
2021-07-29 19:35:16 +0100

    Add method to predict recoil for weapons without

--- Scripts/4_World/entities/firearms/weapon_base.c
+++ Scripts/4_World/entities/firearms/weapon_base.c
@@ -51,4 +51,49 @@ modded class Weapon_Base extends Weapon
 			GetBipodAttachment().DeployBipod(false);
 		}
 	}
+
+	// Override default recoil object
+	override RecoilBase SpawnRecoilObject()
+	{
+		return SpawnPredictedRecoilObject();
+	}
+
+	// This method will try and predict a recoil object for those lazy
+	// modders than didn't give their weapons one
+	protected RecoilBase SpawnPredictedRecoilObject()
+	{
+		if (IsInherited(Pistol_Base) || IsInherited(Magnum_Base))
+			return new Cz75Recoil(this);
+
+		if (IsInherited(BoltActionRifle_InnerMagazine_Base))
+			return new Winchester70Recoil(this);
+		
+		if (IsInherited(BoltActionRifle_Base) || IsInherited(DoubleBarrel_Base))
+			return new CZ527Recoil(this);
+
+		string cartridge_type = GetChamberAmmoTypeName(GetCurrentMuzzle());
+
+		if (cartridge_type.Contains("12ga"))
+			return new Izh43Recoil(this);
+
+		if (cartridge_type.Contains("762x54") || cartridge_type.Contains("308Win"))
+			return new SvdRecoil(this);
+		
+		if (cartridge_type.Contains("Ammo_380"))
+			return new Cz61Recoil(this);
+		
+		if (cartridge_type.Contains("9x39"))
+			return new VSSRecoil(this);
+		
+		if (cartridge_type.Contains("556x45"))
+			return new M4a1Recoil(this);
+
+		if (cartridge_type.Contains("545x39"))
+			return new Ak74Recoil(this);
+
+		if (cartridge_type.Contains("762x39"))
+			return new AkmRecoil(this);
+
+		return new DefaultRecoil(this);
+	}
 }
